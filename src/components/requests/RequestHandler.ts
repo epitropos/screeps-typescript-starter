5c4ca69c7295e9544ecef69b32a850a4e9df7ae6
Garlug <epitropos@gmail.com>
2017-04-27 08:09:32 -0500

    Added the getNextCreepRequest method.

--- src/components/requests/RequestHandler.ts
+++ src/components/requests/RequestHandler.ts
@@ -1,8 +1,14 @@
 // import * as Config from "../../config/config";
-// import {log} from "../../lib/logger/log";
+import {log} from "../../lib/logger/log";
+import {Request} from "./Request";
 
 export class RequestHandler {
   constructor () {
     // No operation.
+  }
+
+  public getNextCreepRequest(creep: Creep): Request {
+    log.info("Retrieving request for creep: " + creep.name);
+    return new Request();
   }
 }
