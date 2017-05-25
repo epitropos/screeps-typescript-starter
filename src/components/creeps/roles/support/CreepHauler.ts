a0161ce458d61a7a6123db503a6dd110f5780eab
Garlug <epitropos@gmail.com>
2017-05-24 22:48:45 -0500

    Commit WIP hauler code
    I am committing WIP hauler code. There is an intentional divide by zero to ensure the code, as is, is not deployed to production.
    Need to add refuel logic to hauler.

--- src/components/creeps/roles/support/CreepHauler.ts
+++ src/components/creeps/roles/support/CreepHauler.ts
@@ -6,6 +6,9 @@ import {RoomHandler} from "../../../rooms/RoomHandler";
 
 export class CreepHauler extends CreepSupport {
   public static MinimumEnergyRequired = 150;
+  public CarriedEnergy: number = 0;
+  public CarriedMinerals: number = 0;
+  public CarriedTotal: number = 0;
 
   public static getBodyParts(energyAvailable: number) {
     if (energyAvailable < CreepHauler.MinimumEnergyRequired) {
@@ -37,69 +40,245 @@ export class CreepHauler extends CreepSupport {
 
   constructor (creep: Creep, roomHandler: RoomHandler) {
     super(creep, roomHandler);
+
+    this.CarriedTotal = _.sum(creep.carry);
+    this.CarriedEnergy = creep.carry[RESOURCE_ENERGY] || 0;
+    this.CarriedMinerals = this.CarriedTotal - this.CarriedEnergy;
+  }
+
+  private determineState() {
+    if (this.CarriedMinerals > 0) {
+      return C.STATE_DELIVERING;
+    }
+
+    if (this.CarriedEnergy > 0) {
+      return C.STATE_DELIVERING;
+    }
+
+    return C.STATE_REFUELING;
+  }
+
+  private deliverEnergy(creep: Creep) {
+    let targetId = creep.memory.targetId;
+    let target = <Structure> Game.getObjectById(targetId);
+    if (!target) {
+      creep.memory.targetId = targetId = this.determineEnergyDeliveryTarget(creep);
+      target = <Structure> Game.getObjectById(targetId);
+    }
+    if (!target) {
+      return;
+    }
+
+    if (this.tryEnergyDropOff(creep, target) === ERR_NOT_IN_RANGE) {
+      this.moveTo(creep, target);
+    }
+  }
+
+  private determineEnergyDeliveryTarget(creep: Creep) {
+    let tower = this.loadTower(creep);
+    if (tower) {
+      return tower.id;
+    }
+
+    let extension = this.loadExtension(creep);
+    if (extension) {
+      return extension;
+    }
+
+    let spawn = this.loadSpawn(creep);
+    if (spawn) {
+      return spawn.id;
+    }
+
+    // let outboundContainer = this.loadOutboundContainer(creep);
+    // if (outBoundContainer) {
+    //   return outBoundContainer.id;
+    // }
+
+    let storage = this.loadStorage(creep);
+    if (storage) {
+      return storage.id;
+    }
+
+    // let terminal = this.loadTerminal(creep);
+    // if (terminal) {
+    //   return terminal.id;
+    // }
+  }
+
+  private deliverMinerals(creep: Creep) {
+    let targetId = creep.memory.targetId;
+    let target = <Structure> Game.getObjectById(targetId);
+    if (!target) {
+      creep.memory.targetId = targetId = this.determineMineralDeliveryTarget(creep);
+      target = <Structure> Game.getObjectById(targetId);
+    }
+    if (!target) {
+      return;
+    }
+
+    if (this.tryResourceDropOff(creep, target) === ERR_NOT_IN_RANGE) {
+      this.moveTo(creep, target);
+    }
+  }
+
+  private determineMineralDeliveryTarget(creep: Creep) {
+    // let terminal = this.loadTerminal(creep);
+    // if (terminal) {
+    //   return terminal.id;
+    // }
+
+    // let labOperandA = this.loadLabOperandA(creep);
+    // if (labOperandA) {
+    //   return labOperandA.id;
+    // }
+
+    // let labOperandB = this.loadLabOperandB(creep);
+    // if (labOperandB) {
+    //   return labOperandB.id;
+    // }
+
+    let storage = this.loadStorage(creep);
+    if (storage) {
+      return storage.id;
+    }
+  }
+
+  private deliver(creep: Creep) {
+    if (this.CarriedMinerals > 0) {
+      this.deliverMinerals(creep);
+      // TODO: deliver minerals.
+      return;
+    }
+
+    if (this.CarriedEnergy > 0) {
+      this.deliverEnergy(creep);
+      return;
+    }
+  }
+
+  private refuel(creep: Creep) {
+    let targetId = creep.memory.targetId;
+    let target = Game.getObjectById(targetId);
+    if (!target) {
+      // TODO: Need to handle dropped resource differently from container.
+      creep.memory.targetId = targetId = this.determineEnergyRefuelTarget(creep);
+      target = Game.getObjectById(targetId);
+    }
+    if (!target) {
+      // TODO: Code goes here.
+      let x = 1 / 0; // TODO: force error so it's impossible to deploy to production.
+    }
+  }
+
+  private determineEnergyRefuelTarget(creep: Creep) {
+    let sourceId = creep.memory.sourceId;
+    let source = <Source> Game.getObjectById(sourceId);
+    if (!source) {
+      // TODO: Figure out what to do here.
+      return;
+    }
+
+    let containers = creep.room.find<Container>(FIND_STRUCTURES, {
+      filter: (c: Container) => c.structureType === STRUCTURE_CONTAINER
+      && (c.store[RESOURCE_ENERGY] || 0) > 0
+      && c.pos.isNearTo(source),
+    });
+    if (containers.length) {
+      return containers[0].id;
+    }
+
+    // let memMinerPosition = creep.room.memory.sources[sourceId].minerPosition;
+    let droppedEnergies = creep.room.find<Resource>(FIND_DROPPED_RESOURCES, {
+      filter: (d: Resource) =>d.resourceType === RESOURCE_ENERGY
+      && d.pos.isNearTo(source),
+    });
+    if (droppedEnergies.length > 0) {
+      return droppedEnergies[0].id;
+    }
   }
 
   public run() {
     super.run();
 
-    let storage = this.loadStorage(this.creep);
-    if (storage) {
-      // Deposit non-energy resources into storage.
-      if (this.isFull(this.creep) && this.carryingNonEnergyResource(this.creep)) {
-        this.tryResourceDropOff(this.creep, storage);
-        return;
-      }
+    let state = this.creep.memory.state;
 
-      // Look for dropped resources.
-      let droppedResource = this.loadDroppedResource(this.creep, 100, this.roomHandler);
-      if (droppedResource !== undefined) {
-        this.moveToPickup(this.creep, droppedResource);
-        return;
-      } else {
-        // Deposit non-energy resources into storage.
-        if (this.carryingNonEnergyResource(this.creep)) {
-          this.tryResourceDropOff(this.creep, storage);
-          return;
-        }
-      }
-    }
-
-    // Default state to refueling.
-    if (this.creep.memory.state === undefined) {
-      this.creep.memory.state = C.STATE_REFUELING;
-    }
-
-    if (this.creep.memory.state === C.STATE_REFUELING) {
-      this.runRefueling(this.creep, this.roomHandler);
-      if (this.doesCreepHaveEnergy(this.creep)) {
-        this.creep.memory.state = C.STATE_DELIVERING;
-      }
+    if (state === C.STATE_TRAVELING) {
       return;
     }
 
-    if (this.creep.memory.state === C.STATE_DELIVERING) {
-      this.runDelivering(this.creep);
-      if (this.isEmpty(this.creep)) {
-        this.creep.memory.state = C.STATE_REFUELING;
-      }
+    this.creep.memory.state = state = this.determineState();
+
+    if (state === C.STATE_DELIVERING) {
+      this.deliver(this.creep);
       return;
     }
 
-    log.error(this.creep.name + " Unknown creep state: " + this.creep.memory.state);
+    if (state === C.STATE_REFUELING) {
+      this.refuel(this.creep);
+      return;
+    }
 
-    // // Default to refueling state.
-    // let energyCarried = this.creep.carry[RESOURCE_ENERGY] || 0;
-    // if (this.creep.memory.state === undefined || energyCarried === 0) {
+    log.error("Un-handled hauler state: " + state);
+
+    // let storage = this.loadStorage(this.creep);
+    // if (storage) {
+    //   // Deposit non-energy resources into storage.
+    //   if (this.isFull(this.creep) && this.carryingNonEnergyResource(this.creep)) {
+    //     this.tryResourceDropOff(this.creep, storage);
+    //     return;
+    //   }
+
+    //   // Look for dropped resources.
+    //   let droppedResource = this.loadDroppedResource(this.creep, 100, this.roomHandler);
+    //   if (droppedResource !== undefined) {
+    //     this.moveToPickup(this.creep, droppedResource);
+    //     return;
+    //   } else {
+    //     // Deposit non-energy resources into storage.
+    //     if (this.carryingNonEnergyResource(this.creep)) {
+    //       this.tryResourceDropOff(this.creep, storage);
+    //       return;
+    //     }
+    //   }
+    // }
+
+    // // Default state to refueling.
+    // if (this.creep.memory.state === undefined) {
     //   this.creep.memory.state = C.STATE_REFUELING;
-    // } else if (energyCarried > 0) {
-    //   this.creep.memory.state = C.STATE_DELIVERING;
     // }
 
-    // switch (this.creep.memory.state) {
-    //   case C.STATE_REFUELING: this.runRefueling(this.creep, this.roomHandler); return;
-    //   case C.STATE_DELIVERING: this.runDelivering(this.creep); return;
-    //   default: log.error("Unknown creep state: " + this.creep.memory.state);
+    // if (this.creep.memory.state === C.STATE_REFUELING) {
+    //   this.runRefueling(this.creep, this.roomHandler);
+    //   if (this.doesCreepHaveEnergy(this.creep)) {
+    //     this.creep.memory.state = C.STATE_DELIVERING;
+    //   }
+    //   return;
     // }
+
+    // if (this.creep.memory.state === C.STATE_DELIVERING) {
+    //   this.runDelivering(this.creep);
+    //   if (this.isEmpty(this.creep)) {
+    //     this.creep.memory.state = C.STATE_REFUELING;
+    //   }
+    //   return;
+    // }
+
+    // log.error(this.creep.name + " Unknown creep state: " + this.creep.memory.state);
+
+    // // // Default to refueling state.
+    // // let energyCarried = this.creep.carry[RESOURCE_ENERGY] || 0;
+    // // if (this.creep.memory.state === undefined || energyCarried === 0) {
+    // //   this.creep.memory.state = C.STATE_REFUELING;
+    // // } else if (energyCarried > 0) {
+    // //   this.creep.memory.state = C.STATE_DELIVERING;
+    // // }
+
+    // // switch (this.creep.memory.state) {
+    // //   case C.STATE_REFUELING: this.runRefueling(this.creep, this.roomHandler); return;
+    // //   case C.STATE_DELIVERING: this.runDelivering(this.creep); return;
+    // //   default: log.error("Unknown creep state: " + this.creep.memory.state);
+    // // }
   }
 
   public runRefueling(creep: Creep, roomHandler: RoomHandler) {
